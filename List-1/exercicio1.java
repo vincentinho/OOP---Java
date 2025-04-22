630b1a875e9b1d0acf9f4beac9312f8e48387329
Vicente Ferran <107846617+vincentinho@users.noreply.github.com>
2025-04-22 18:29:08 -0300

    Add files via upload

---
+++ List-1/exercicio1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class nomes{
+        public static void main(String args[]){
+        System.out.println("Digite seu nome");
+        Scanner scanner = new Scanner(System.in);
+        String nome = scanner.nextLine();
+        System.out.println("O seu nome é " + nome);
+    }
+}
